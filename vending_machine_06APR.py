7bde69c37fd2ebf8de44158f1315455db97ae591
manugent1 <81817307+manugent1@users.noreply.github.com>
2021-04-06 16:30:03 +0100

    Update vending_machine_06APR.py

--- vending_machine_06APR.py
+++ vending_machine_06APR.py
@@ -20,9 +20,9 @@ DHT_READ_TIMEOUT = 10
 # Pin connected to DHT11, Luminosity and PUM data pin - @Put here whats is the right number
 DHT_DATA_PIN = 17
 PUMP_DATA_PIN = 37
-LDR_DATA_PIN = 18
-LIGHT_DATA_PIN = 35
-FAN_DATA_PIN = 33
+LDR_DATA_PIN = 12
+LIGHT_DATA_PIN = 13
+FAN_DATA_PIN = 15
 
 #Setup of GPIO to work with BOARD PIN mode and OUTPUT
 GPIO.setmode(GPIO.BOARD)
@@ -33,10 +33,10 @@ GPIO.setup(FAN_DATA_PIN,GPIO.OUT)
 
 # Set Adafruit IO key.
 # the key is secret so not publish this when we publish this code!
-ADAFRUIT_IO_KEY = 'xxxxx'
+ADAFRUIT_IO_KEY = '******'
 
 # Set  Adafruit IO username.
-ADAFRUIT_IO_USERNAME = 'xxxxxx'
+ADAFRUIT_IO_USERNAME = '*****'
 
 #Set the ID of the feed to subscribe to for updates.
 PUMP_ID = 'pump'
@@ -74,19 +74,19 @@ def message(client, topic_id, payload):
     # the new value.
     print('Topic {0} received new value: {1}'.format(topic_id, payload))
     if topic_id == 'pump':
-        if payload == 'ON':
+        if payload == 'OFF':
             GPIO.output(PUMP_DATA_PIN, GPIO.HIGH)
-        elif payload == 'OFF':
+        elif payload == 'ON':
             GPIO.output(PUMP_DATA_PIN, GPIO.LOW)
     if topic_id == 'light':
-        if payload == 'ON':
+        if payload == 'OFF':
             GPIO.output(LIGHT_DATA_PIN, GPIO.HIGH)
-        elif payload == 'OFF':
+        elif payload == 'ON':
             GPIO.output(LIGHT_DATA_PIN, GPIO.LOW)
     if topic_id == 'fan':
-        if payload == 'ON':
+        if payload == 'OFF':
             GPIO.output(FAN_DATA_PIN, GPIO.HIGH)
-        elif payload == 'OFF':
+        elif payload == 'ON':
             GPIO.output(FAN_DATA_PIN, GPIO.LOW)
 # Create an instance of MQTT client
 
@@ -116,11 +116,28 @@ luminosity_feed = 'luminosity'
 # Set up DHT11 Sensor.
 dht11_sensor = Adafruit_DHT.DHT11
 
+def rc_time (LDR_DATA_PIN):
+    count = 0
+  
+    #Output on the pin for 
+    GPIO.setup(LDR_DATA_PIN, GPIO.OUT)
+    GPIO.output(LDR_DATA_PIN, GPIO.LOW)
+    time.sleep(0.1)
+
+    #Change the pin back to input
+    GPIO.setup(LDR_DATA_PIN, GPIO.IN)
+  
+    #Count until the pin goes high
+    while (GPIO.input(LDR_DATA_PIN) == GPIO.LOW):
+        count += 1
+
+    return count
+
 while True:
     #For when we put the Raspberry in the loop:
     humidity, temperature = Adafruit_DHT.read_retry(dht11_sensor, DHT_DATA_PIN)
     #humidity,temperature,luminosity = 30,25,620
-    luminosity = 620
+    luminosity = rc_time(LDR_DATA_PIN)
     if humidity is not None and temperature is not None:
         print('Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(temperature, humidity))
         # Send humidity and temperature feeds to Adafruit IO
